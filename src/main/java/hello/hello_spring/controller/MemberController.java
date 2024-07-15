32ce493cb354ec8c23d5e51ab2aafe8573e27a8b
gongonii <pje6217@naver.com>
2024-07-16 03:00:16 +0900

    section 5-3:회원조회

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,11 +1,16 @@
 package hello.hello_spring.controller;
 
+
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
 
 @Controller
 public class MemberController {
@@ -39,6 +44,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";//회원가입이 끝나면 홈화면으로 보내기 "/"이게 홈화면
+    }
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members =  memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberlist";
     }
 
 
