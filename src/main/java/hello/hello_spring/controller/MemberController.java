ea4122b7ce8b1b7d6a61fb628b65b6655006f1a2
gongonii <pje6217@naver.com>
2024-07-16 01:52:33 +0900

    section 4-2:자바코드로 스프링빈 등록하기

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -7,13 +7,22 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
 
-    //private final MemberService memberService = new MemberService(); //하나로 생성하고 공통으로 사용해도 된다. 그럴려면 스프링 컨테이너에 등록을 하면 됨. 그게 바로 밑에 방식
-    private final MemberService memberService;
+    private MemberService memberService;
 
     @Autowired //멤버서비스를 스프링 컨테이너에 있는 멤버서비스를 가져다가 연결을 시켜주는 역할
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     } //생성자를 만들어준다.
+
+
+    //private final MemberService memberService = new MemberService(); //하나로 생성하고 공통으로 사용해도 된다. 그럴려면 스프링 컨테이너에 등록을 하면 됨. 그게 바로 밑에 방식
+   // @Autowired  private final MemberService memberService; 필드 주입은 바꿀 수 없다는 단점,
+
+    //@Autowired //setter Injection방식 단점- 누군가가 멤버 컨트롤을 호출했을 때 퍼블릭으로 열려있어야함
+    //public void setMemberService(MemberService memberService) {
+    //    this.memberService = memberService;
+    //}
+
 
 
 
--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class MemberService {
 
 
     private final MemberRepository memberRepository;
 
-    @Autowired
+
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;// 멤버 서비스를 멤버 리포지토리를 직접 내가 new에서 생성하는게 아니라 외부에서 넣어주도록 함
     }
